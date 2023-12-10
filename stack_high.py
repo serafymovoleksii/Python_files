c4b42df6cf74877db84fc22ffd29b438a461f60d
serafymovoleksii <145356215+serafymovoleksii@users.noreply.github.com>
2023-12-10 10:56:05 -0500

    Add files via upload

--- stack_high.py
+++ stack_high.py
@@ -1,1 +1,13 @@
-"# Python_files" 
+def stack_high(num1):
+     if num1 == 0:
+         return num1
+     print("Current count:",num1)
+     num1 += 1
+     stack_high(num1)
+
+def main() :
+     num1 = stack_high(1)
+     print("Current count:",num1)
+
+if __name__ == "__main__":
+     main()
